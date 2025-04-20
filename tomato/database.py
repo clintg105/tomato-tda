a4e914b228710f6f4b6f6b5280952c4741732c7e
Clint . <clintengraham@gmail.com>
2025-04-20 16:17:51 -0400

    adding metrics to database and restructuring some

--- tomato/database.py
+++ tomato/database.py
@@ -11,6 +11,7 @@ import pandas as pd
 
 from tomato.utils import load_critic_review_df, tomato_data_path
 from tomato.encoding import bert_encode_reviews
+from tomato.metrics import pdist2, wasserstein_distances_sinkhorn_parallel
 
 class Mode(Enum):
     """What should happen if the artefact is missing on disk?"""
@@ -24,16 +25,13 @@ class TDAManager:
     Interface to on‑disk cache.
 
     Root
-    └─ Encodings/{dataset}/
+    └─ {dataset}/
        ├─ encoding.parquet
-       ├─ distances/{metric}.npy
+       ├─ metric/{metric}.npy
        └─ tda/
           ├─ full/{metric}-{downsample}.npz
           └─ split/{split}/{metric}-{downsample}.npz
     """
-    _re_metric   = re.compile(r"^[a-z0-9_]+$")
-    _re_down     = re.compile(r"^[a-zA-Z0-9]+$")
-    _re_split    = re.compile(r"^[a-zA-Z0-9_\-]+$")
 
     # construction
     def __init__(self):
@@ -81,19 +79,15 @@ class TDAManager:
         Attempt to hydrate *K* directly from disk; raise FileNotFoundError
         if the expected file is not present.
         """
-        if len(K) == 1:
-            if K[0] == "df_critic":
-                return load_critic_review_df()
-        else:
-            enc_dir = self.root / K[0]
+        enc_dir = self.root / K[0]
 
-            if len(K) == 2 and K[1] == "encoding":
-                f = enc_dir / "encoding.parquet"
-                return pd.read_parquet(f) # raises if missing
+        if len(K) == 2 and K[1] == "encoding":
+            f = enc_dir / "encoding.parquet"
+            return pd.read_parquet(f) # raises if missing
 
-            if len(K) == 3 and K[1] == "distances": # .npy
-                f = self._enc_dir(K[0]) / "distances" / f"{K[2]}.npy"
-                return np.load(f) # raises if missing
+        if len(K) == 3: # .npy
+            f = enc_dir / K[1] / "metric" / f"{K[2]}.npy"
+            return np.load(f) # raises if missing
 
         raise FileNotFoundError  # unknown pattern – treat as “not on disk”
 
@@ -102,7 +96,12 @@ class TDAManager:
         Dispatch to a concrete builder.
         TODO: Make branches single‑line calls to “_make_*” helpers.
         """
+        # "global" data
+        if len(K) == 1:
+            if K[0] == "df_critic":
+                return load_critic_review_df()
         
+        # downsampled/encoded data
         if len(K) == 2:
             enc, downsample = K[0].split("_")
             if K[1] == "df_critic":
@@ -115,7 +114,7 @@ class TDAManager:
                 if key == "unif":
                     return df_full.sample(num,random_state=42)
                 else:
-                    raise ValueError(f"unknown sample spec '{key}'")
+                    raise ValueError(f"Unknown sample spec '{key}'")
             elif K[1] == "encoding":
                 df_small = self.get(K[0],'df_critic')
                 if enc == "bert":
@@ -126,8 +125,10 @@ class TDAManager:
                     df_enc.to_parquet(path, compression='snappy')
                     return df_enc
                 else:
-                    raise ValueError(f"unknown encoding spec '{enc}'")
-            elif K[1] == "pooled_vectors":
+                    raise ValueError(f"Unknown encoding spec '{enc}'")
+            # Everything below requires encoding and is used in base metrics 
+            # (this should not impact wasserstein metrics)
+            elif K[1] == "pooled":
                 df_enc = self.get(K[0], "encoding")
                 dim_cols = [c for c in df_enc.columns if c.startswith('dim_')]
                 mean_vectors = np.vstack(
@@ -137,12 +138,69 @@ class TDAManager:
                             g[dim_cols].values, weights=g.attention_mask, axis=0))\
                         .values)
                 return mean_vectors
+            elif K[1] == "cls":
+                df_enc = self.get(K[0], "encoding")
+                dim_cols = [c for c in df_enc.columns if c.startswith('dim_')]
+                cls_vectors = df_enc[df_enc['token_id'] == 0][dim_cols].values
+                return cls_vectors
+            else:
+                ValueError(f"Unrecognised reduction method: {K[1]}")
 
         
+        # metric data
         if len(K) == 3:
-            if K[1] == "metric":
-                mean_vectors = self.get(K[0], "pooled_vectors")
+            spec = K[2]
+            wass_check = contains_re(spec, ['wass', r'ws\d+'])
+            geod_check = contains_re(spec, [r'\d+nn'])
+            if geod_check[0]:
+                spec_base = geod_check[3]
+                X = self.get(K[0], K[1], spec_base)
+
+                n = int(re.fullmatch(r"(\d+)nn", geod_check[2]).group(1))
+            elif wass_check[0]:
+                # TODO: not impacted by K[1] (recheck other subdirs?)
+                df_enc = self.get(K[0], "encoding")
+                spec_base = wass_check[3]
+                args = get_pdist2_args_from_base(spec_base)
+
+                wass_spec = wass_check[2]
+                if wass_spec == "ws01":
+                    D = wasserstein_distances_sinkhorn_parallel(df_enc, *args, 0.01)
+                else:
+                    raise ValueError(f"unknown encoding spec '{spec}'")
+                Path(path).parent.mkdir(parents=True, exist_ok=True)
+                np.save(path, D)
+            else:
+                # base metric
+                X = self.get(K[0], K[1])
+                args = get_pdist2_args_from_base(spec)
+                D = pdist2(X,X,*args)
+            return D
+
+        # TDA data
             
         raise KeyError(f"Unrecognised path: {K!r}")
 
+def get_pdist2_args_from_base(spec):
+    if spec == "cos":
+        return ["cosine"]
+    elif spec == "mp1":
+        return ["minkowski",1]
+    else:
+        raise ValueError(f"unknown encoding spec '{spec}'")
+    
 
+
+def contains_re(s, pats):
+    """
+    Return (True, pat, match, without_match) if any _-split part fully matches a pattern.
+    Otherwise return (False, None, s, s).
+    """
+    parts = s.split('_')
+    for i, x in enumerate(parts):
+        for p in pats:
+            if re.fullmatch(p, x):
+                match = parts[i]
+                without_match = '_'.join(parts[:i] + parts[i+1:])
+                return True, p, match, without_match
+    return False, None, s, s
